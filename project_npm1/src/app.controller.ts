6ebcd3278d16a22b4bb58774c6da302c4dca973d
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-06-23 17:28:17 +0900

    0623

--- project_npm1/src/app.controller.ts
+++ project_npm1/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
+  // * localhost:8000/cats/hello
+  @Get('hello/:id/:name')
+  getHello(
+    @Req() req: Request,
+    @Body() Body,
+    @Param() param: { id: string; name: string },
+  ): string {
+    // console.log(req);
+    console.log(param);
     return this.appService.getHello();
   }
 }
