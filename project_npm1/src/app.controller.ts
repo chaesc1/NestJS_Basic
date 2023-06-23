d56431838ad2ae6b90373ad93908b9b799f4aa96
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-06-23 18:03:28 +0900

    0623

--- project_npm1/src/app.controller.ts
+++ project_npm1/src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
 
 @Controller('cats')
 export class AppController {
-  constructor(private readonly appService: AppService) {} //의존성 주입 패턴
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {} //의존성 주입 패턴
 
   @Get()
   getHello(): string {
---
+++ project_npm1/src/cats/cats.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+@Module({
+  controllers: [CatsController],
+  providers: [CatsService],
+  exports: [CatsService], //외부에서 사용가능하게 exports 해주는것
+})
+export class CatsModule {}
