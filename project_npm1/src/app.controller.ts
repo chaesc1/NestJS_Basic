dcbb5a9191f722fb52682e0fa894562e1b6d6fb5
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-06-23 18:46:00 +0900

    0623

--- project_npm1/src/app.controller.ts
+++ project_npm1/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CatsService } from './cats/cats.service';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
--- project_npm1/src/app.module.ts
+++ project_npm1/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { UsersModule } from './users/users.module';
+import { CatsService } from './cats/cats.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [CatsModule], //다른 모듈에서 만들어 진 것들은 import
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
