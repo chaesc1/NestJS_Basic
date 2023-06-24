1a2286618d352070adfb9ea6c9ef392fa18362ff
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-06-24 16:59:05 +0900

    0624

---
+++ project_npm1/src/http-exception.filter.ts
@@ -0,0 +1,36 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp(); //실행환경에서
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const error = exception.getResponse() as
+      | string
+      | { error: string; statusCode: number; message: string | string[] };
+
+    /// res.status(400).semd({asdasd;})
+    if (typeof error === 'string') {
+      response.status(status).json({
+        success: false,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        error,
+      });
+    } else {
+      response.status(status).json({
+        success: false,
+        timestamp: new Date().toISOString(),
+        ...error,
+      });
+    }
+  }
+}
