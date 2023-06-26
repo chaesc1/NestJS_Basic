b4812170eadb04d0f237985be40314d5ebda88c0
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-06-26 21:31:20 +0900

    0626

--- project_npm1/src/cats/cats.schema.ts
+++ project_npm1/src/cats/cats.schema.ts
@@ -31,7 +31,19 @@ export class Cat extends Document {
   password: string;
 
   @Prop()
+  @IsString()
   imgUrl: string;
+
+  readonly readOnlyData: { id: string; email: string; name: string };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
+
+// 클라이언트에게 보여지는 데이터만 추상화한다.
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+  };
+});
--- project_npm1/src/cats/cats.service.ts
+++ project_npm1/src/cats/cats.service.ts
@@ -6,8 +6,9 @@ import {
 import { CatRequestDto } from 'src/dto/cats.request.dto';
 import { Cat } from './cats.schema';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+
 @Injectable()
 export class CatsService {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
@@ -24,5 +25,13 @@ export class CatsService {
 
     //패스워드 암호화
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    const cat = await this.catModel.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    return cat.readOnlyData;
   }
 }
